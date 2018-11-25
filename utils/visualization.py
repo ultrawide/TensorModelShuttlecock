2c064b261417d8bd5da25ebad7cb9ef32e3680f2
Colin <colinchan02@gmail.com>
2018-11-24 16:37:00 -0800

    nov 24 2018 tested live stream with birdie test 1

--- utils/visualization.py
+++ utils/visualization.py
@@ -93,10 +93,12 @@ class BBoxVisualization():
         for bb, cf, cl in zip(box, conf, cls):
             cl = int(cl)
             y_min, x_min, y_max, x_max = bb[0], bb[1], bb[2], bb[3]
-            color = self.colors[cl]
+            color = (0, 255, 0)
+            #color = self.colors[cl]
             cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color, 2)
             txt_loc = (max(x_min+2, 0), max(y_min+2, 0))
-            cls_name = self.cls_dict.get(cl, 'CLS{}'.format(cl))
+            #cls_name = self.cls_dict.get(cl, 'CLS{}'.format(cl))
+            cls_name = 'Shuttlecock'
             txt = '{} {:.2f}'.format(cls_name, cf)
             img = draw_boxed_text(img, txt, txt_loc, color)
         return img
